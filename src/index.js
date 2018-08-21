f86db7849248aecd77091cd3934c8680d7f64b82
Victor Kozlov <punkrus18@gmail.com>
2018-08-21 14:13:33 +0300

    chore(configs)
    refactor: use config file for session and secrets;
    refactor: rename config.sessionSecretKeys => appSecrets;

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 const Koa = require('koa')
 const BodyParser = require('koa-bodyparser')
 const session = require('koa-session')
-const { server: serverConfig } = require('../config')
+const {
+  appSecrets,
+  server: serverConfig,
+  session: sessionConfig
+} = require('../config')
 const router = require('./routes/index')
-
 const app = new Koa()
-app.keys = ['chat.secret']
 
-const CONFIG = {
-  key: 'chat:sess',
-  maxAge: 86400000,
-  overwrite: true, /** (boolean) can overwrite or not (default true) */
-  httpOnly: true, /** (boolean) httpOnly or not (default true) */
-  signed: true, /** (boolean) signed or not (default true) */
-  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
-  renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false) */
-}
+app.keys = appSecrets
 
-app.use(session(CONFIG, app))
+app.use(session(sessionConfig, app))
 app.use(BodyParser())
 app.use(router.routes())
 
