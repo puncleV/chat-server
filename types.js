162b7f5c98e12b2571c15fef05b9398e2a073e4d
Victor Kozlov <punkrus18@gmail.com>
2018-08-22 16:20:01 +0300

    feat(database)
    add: message type structure;

--- types.js
+++ types.js
@@ -2,7 +2,7 @@
  * @typedef {object} User
  * @property {mongo.ObjectID} _id - user id
  * @property {string} username username
- * @property {Array.<string>} rooms room keys
+ * @property {Array.<string>} privateRooms private room keys
  * @property {boolean} online online status
  */
 
@@ -12,6 +12,15 @@
  * @property {string} name room name
  * @property {string} creator room creator
  * @property {RoomType} type rooms type
+ */
+
+/**
+ * @typedef {object} Message
+ * @property {mongo.ObjectID} _id - message id
+ * @property {string} to message to (all - to all)
+ * @property {string} from message from
+ * @property {string} text message text
+ * @property {mongo.ObjectID} room id
  */
 
 /**
