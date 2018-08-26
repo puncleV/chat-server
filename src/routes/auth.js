09d662ea88f9981674bb054cb2b1f6b691909ca3
Victor Kozlov <punkrus18@gmail.com>
2018-08-26 13:28:11 +0300

    refactor(router)
    refactor: split auth routes to auth router;

---
+++ src/routes/auth.js
@@ -0,0 +1,73 @@
+const Router = require('koa-router')
+
+const ResponseHelper = require('../helpers/response-helper')
+const { auth: {usernameMaxLength, baseRoute} } = require('../../config')
+const UserApi = require('../db/user-api')
+const router = new Router()
+const authRouter = new Router()
+
+router.post(`/login`, async ctx => {
+  const username = ctx.request.body.username
+
+  ctx.body = {
+    status: 'success'
+  }
+
+  if (typeof username !== 'string' || username.length === 0) {
+    return ResponseHelper.error(ctx, 400, 'wrong username')
+  }
+
+  if (username.length > usernameMaxLength) {
+    return ResponseHelper.error(ctx, 400, 'wrong username length')
+  }
+
+  if (ctx.session.username === username) {
+    return
+  } else if (typeof ctx.session.username === 'string') {
+    return ResponseHelper.error(ctx, 403, 'access forbidden')
+  }
+
+  try {
+    const userCollection = await ctx.app.db.collection('users')
+
+    await UserApi.login(userCollection, username)
+
+    ctx.session.username = username
+  } catch (e) {
+    return ResponseHelper.error(ctx, 500, 'server error')
+  }
+})
+
+router.get(`/checkLogin`, async ctx => {
+  if (void 0 !== ctx.session.username) {
+    ctx.body = {
+      status: 'success'
+    }
+  } else {
+    return ResponseHelper.error(ctx, 401, 'unauthorized')
+  }
+})
+
+router.get(`/logout`, async ctx => {
+  if (typeof ctx.session.username !== 'string') {
+    return ResponseHelper.error(ctx, 401, 'unauthorized')
+  }
+
+  try {
+    const userCollection = await ctx.app.db.collection('users')
+
+    await UserApi.logout(userCollection, ctx.session.username)
+  } catch (e) {
+    return ResponseHelper.error(ctx, 500, 'server error')
+  }
+
+  ctx.session = null
+
+  ctx.body = {
+    status: 'success'
+  }
+})
+
+authRouter.use(baseRoute, router.routes(), router.allowedMethods())
+
+module.exports = authRouter
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,75 +1,9 @@
 const Router = require('koa-router')
 
-const ResponseHelper = require('../helpers/response-helper')
 const { baseApiRoute } = require('../../config')
-const { usernameMaxLength } = require('../../config')
-const UserApi = require('../db/user-api')
-
-const router = new Router()
+const authRouter = require('./auth')
 const baseRouter = new Router()
 
-router.post(`/login`, async ctx => {
-  const username = ctx.request.body.username
-
-  ctx.body = {
-    status: 'success'
-  }
-
-  if (typeof username !== 'string' || username.length === 0) {
-    return ResponseHelper.error(ctx, 400, 'wrong username')
-  }
-
-  if (username.length > usernameMaxLength) {
-    return ResponseHelper.error(ctx, 400, 'wrong username length')
-  }
-
-  if (ctx.session.username === username) {
-    return
-  } else if (typeof ctx.session.username === 'string') {
-    return ResponseHelper.error(ctx, 403, 'access forbidden')
-  }
-
-  try {
-    const userCollection = await ctx.app.db.collection('users')
-
-    await UserApi.login(userCollection, username)
-
-    ctx.session.username = username
-  } catch (e) {
-    return ResponseHelper.error(ctx, 500, 'server error')
-  }
-})
-
-router.get(`/checkLogin`, async ctx => {
-  if (void 0 !== ctx.session.username) {
-    ctx.body = {
-      status: 'success'
-    }
-  } else {
-    return ResponseHelper.error(ctx, 401, 'unauthorized')
-  }
-})
-
-router.get(`/logout`, async ctx => {
-  if (typeof ctx.session.username !== 'string') {
-    return ResponseHelper.error(ctx, 401, 'unauthorized')
-  }
-
-  try {
-    const userCollection = await ctx.app.db.collection('users')
-
-    await UserApi.logout(userCollection, ctx.session.username)
-  } catch (e) {
-    return ResponseHelper.error(ctx, 500, 'server error')
-  }
-
-  ctx.session = null
-
-  ctx.body = {
-    status: 'success'
-  }
-})
-
-baseRouter.use(baseApiRoute, router.routes(), router.allowedMethods())
+baseRouter.use(baseApiRoute, authRouter.routes(), authRouter.allowedMethods())
 
 module.exports = baseRouter
